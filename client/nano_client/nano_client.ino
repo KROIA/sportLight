36cdc23f445f5fef5f3e766ab9dc1fe760d4ae02
Alex Krieg <alexkrieg@gmx.ch>
2018-06-26 18:26:07 +0200

    v0.2.0
    Adding:
    voltage sensor
    MPU6050 calibrate

--- client/nano_client/nano_client.ino
+++ client/nano_client/nano_client.ino
@@ -1,7 +1,7 @@
 /*
 Autor Alex Krieg
-Datum 20.06.2018
-Version 0.1.1
+Datum 26.06.2018
+Version 0.2.0
 */
 
 #include <Adafruit_NeoPixel.h>
@@ -19,6 +19,7 @@ Version 0.1.1
 
 #define NUMPIXELS 16
 #define PIXELPIN 2
+#define AKKUPIN A1
 
 //#define DEBUG
 
@@ -37,7 +38,8 @@ struct color
 enum modus
 {
   modus_none = 0,
-  modus_reflex = 1
+  modus_reflex = 1,
+  modus_akkuLow = 2
 };
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIXELPIN, NEO_GRB + NEO_KHZ800);
@@ -51,16 +53,32 @@ Timer wifiServerUpdateTimer;
 //---------------------------MODE
 Timer mode_reflex_Timer;
 unsigned int mode_reflex_time = 0;
+
+//---------
+bool mode_akkuLow_dimUp = true;
+Timer mode_akkuLowTimer;
+byte mode_akkuLow_colorRed = 0;
+byte mode_akkuLow_numLedOn = 1;
 //------------------------------
 
-const float accelerationTrigger = 1.0;
-float accelerationAverage = 0;
+const float accelerationTrigger = 3.0;
+float gesAcceleration = 0;
+float akkuVoltage = 11.00;
+float minAkkuVoltage = 7.0; // 7.0V
+Timer checkAkkuTimer;
+
+int loadingPos = 1;
 
 accel acceleration;
 color pixels_color;
 bool  pixels_enable = false;
 modus mode = modus_none;
 modus lastMode = modus_none;
+
+float tmpAX = 0;
+float tmpAY = 0;
+float tmpAZ = 0;
+Timer loadingTimer;
 
 void readSerial();
 void wifiUpdate();
@@ -79,6 +97,8 @@ modus LASTMODE();
 void setup()
 {
   Serial.begin(115200);
+  Serial.print("version: ");
+  Serial.println("0.2.0");
   accelgyro.initialize();
   #if defined (__AVR_ATtiny85__)
     if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
@@ -88,23 +108,68 @@ void setup()
     Serial.println("Testing device connections...");
     Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
   #endif
+
+  pinMode(AKKUPIN,INPUT);
     
   sensor_UpdateIntervalTimer.onFinished(sensorUpdateFunction);
   sensor_UpdateIntervalTimer.autoRestart(true);
-  sensor_UpdateIntervalTimer.start(50);
+  sensor_UpdateIntervalTimer.start(20);
   irSensorCooldownTimer.onFinished(irSensorCooldownTimerFinishedFunction);
   pixelsUpdateTimer.onFinished(updateRGB);
   pixelsUpdateTimer.autoRestart(true);
   pixelsUpdateTimer.start(100);
   wifiServerUpdateTimer.onFinished(wifiUpdate);
-  wifiServerUpdateTimer.autoRestart(false);
+  wifiServerUpdateTimer.autoRestart(true);
   wifiServerUpdateTimer.start(3000);
+
+  checkAkkuTimer.onFinished(checkAkku);
+  checkAkkuTimer.autoRestart(true);
+  checkAkkuTimer.start(50);
   
   ir_sensor.onHighTrigger(IR_highTriggerFunction);
 
   pixels_color.red = 10;
   pixels_color.green = 10;
   pixels_color.blue = 10;
+
+
+
+    /*accelgyro.setXAccelOffset(-2210);
+    accelgyro.setYAccelOffset(-600);
+    accelgyro.setZAccelOffset(1702);*/
+/*
+  accelgyro.setXAccelOffset(-2212);
+  accelgyro.setYAccelOffset(-648);
+  accelgyro.setZAccelOffset(2585);
+
+  accelgyro.setXGyroOffset(6);
+  accelgyro.setYGyroOffset(65);
+  accelgyro.setZGyroOffset(-23);
+*/
+/*
+  accelgyro.setXAccelOffset(-4550);
+  accelgyro.setYAccelOffset(-1601);
+  accelgyro.setZAccelOffset(1252);
+
+  accelgyro.setXGyroOffset(150);
+  accelgyro.setYGyroOffset(-47);
+  accelgyro.setZGyroOffset(2);
+
+  accelgyro.setFullScaleAccelRange(2);
+*/
+    /*
+    accelgyro.setXAccelOffset(-2159);
+    accelgyro.setYAccelOffset(-664);
+    accelgyro.setZAccelOffset(1650);
+    */
+  
+  calibration();
+  /*
+  Serial.print(accelgyro.getXAccelOffset()); Serial.print("\t"); // -76       -2159
+  Serial.print(accelgyro.getYAccelOffset()); Serial.print("\t"); // -2359   -664
+  Serial.print(accelgyro.getZAccelOffset()); Serial.print("\t"); // 1688    1788
+  Serial.print("\n");*/
+ // delay(1000);
 }
 
 void loop()
@@ -113,11 +178,16 @@ void loop()
   irSensorCooldownTimer.update();
   sensor_UpdateIntervalTimer.update();
   wifiServerUpdateTimer.update();
+  checkAkkuTimer.update();
   handleMode();
   readSerial();
 }
 void wifiUpdate()
 {
+  if(mode_reflex_Timer.isRunning())
+  {  
+    return;
+  }
   Serial.print("color]");
   //-------------einzeltest
   pixels_color.red = random(0,100);
@@ -125,7 +195,7 @@ void wifiUpdate()
   pixels_color.blue = random(0,100);
   MODE(modus_reflex);
   //----------------------
-  wifiServerUpdateTimer.start(3000);
+  
 }
 
 void readSerial()
@@ -137,7 +207,7 @@ void readSerial()
       Serial.print("serialRead: ");
       Serial.println(inputBuffer);
     #endif
-    if(inputBuffer.indexOf("color") != -1)  // color|100|100|100|]
+    if(inputBuffer.indexOf("color") != -1)  // color|100|100|100|
     {
       inputBuffer = inputBuffer.substring(inputBuffer.indexOf("|")+1);
       pixels_color.red = atoi(inputBuffer.substring(0,inputBuffer.indexOf("|")).c_str());
@@ -147,19 +217,36 @@ void readSerial()
       pixels_color.blue = atoi(inputBuffer.substring(0,inputBuffer.indexOf("|")).c_str());
       MODE(modus_reflex);
     }
+    if(inputBuffer.indexOf("data") != -1)  //data|
+    {
+      inputBuffer = inputBuffer.substring(inputBuffer.indexOf("|")+1);
+      
+    }
   }
 }
 void sensorUpdateFunction()
 {
   ir_sensor.update();
-  accelgyro.getAcceleration(&acceleration.x,&acceleration.y,&acceleration.z);
-  //                                                                                                      9.81*2048
-  accelerationAverage = ((float)abs(acceleration.x)+(float)abs(acceleration.y)+(float)abs(acceleration.z)-15357.5424)/(3*2048);
-  #ifdef DEBUG
-    Serial.print("accelerationAverage: ");
-    Serial.println(accelerationAverage);
-   #endif
-   if(accelerationAverage > accelerationTrigger)
+  //accelgyro.getAcceleration(&acceleration.x,&acceleration.y,&acceleration.z);
+  getAcceleration(accelgyro,tmpAX,tmpAY,tmpAZ);
+  gesAcceleration = sqrt(tmpAX*tmpAX+tmpAY*tmpAY+tmpAZ*tmpAZ)-9.804;
+ // #ifdef DEBUG
+ /*Serial.print(ax);
+ Serial.print("\t");
+
+  Serial.print(ay);
+ Serial.print("\t");
+  Serial.print(az);
+  Serial.print("\t");
+  Serial.println(gesAcceleration);*/
+// Serial.print("\t");
+ //Serial.print("\n");
+   // Serial.print("gesAcceleration: ");
+   // Serial.println(gesAcceleration);
+ //  Serial.println(gesAcceleration);
+ //  #endif
+ //Serial.println(gesAcceleration);
+   if(gesAcceleration > accelerationTrigger)
    {
       switch(MODE())
       {
@@ -170,6 +257,10 @@ void sensorUpdateFunction()
         }
         case modus_reflex:
         {
+          #ifdef DEBUG
+            Serial.print("\t accel: ");
+            Serial.println(gesAcceleration);
+          #endif
           triggerFunction();
           break;
         }
@@ -247,6 +338,33 @@ void handleMode()
       
       break;
     }
+    case modus_akkuLow:
+    {
+      
+     /* pixels_enable = true;
+      if(mode_akkuLowTimer.start(10))
+      {
+        if(mode_akkuLow_dumUp)
+        {
+          pixels_color.red++;
+        }
+        else
+        {
+          pixels_color.red--;
+        }
+      }
+
+      if(mode_akkuLow_colorRed >= 50)
+      {
+        mode_akkuLow_dumUp = false;
+      }else if(mode_akkuLow_colorRed <= 0)
+      {
+        mode_akkuLow_dumUp = true;
+      }*/
+
+      LED_akku();
+      break;
+    }
   }
 }
 
@@ -263,5 +381,206 @@ modus MODE()
 modus LASTMODE()
 {
   return lastMode;
+}
+
+
+
+void calibration()
+{
+  //Serial.println("calibration");
+  float ax = 0;
+  float ay = 0;
+  float az = 0;
+  float endError = 0.5;
+  float error = 0;
+  float aGes = 0;
+
+  float toAX = 0;
+  float toAY = 0;
+  float toAZ = 9.804;
+  for(int a=0; a<10; a++)
+  {
+    getAcceleration(accelgyro,tmpAX,tmpAY,tmpAZ);
+    aGes += sqrt(tmpAX*tmpAX+tmpAY*tmpAY+tmpAZ*tmpAZ);
+    delay(5);
+  }
+  aGes /= 10;
+  int map_firstError = sqrt((aGes - toAZ)*(aGes - toAZ)) * 150;
+  //Serial.print("map_firstError: ");
+  //Serial.println(map_firstError);
+  bool ret = false;
+  while(!ret)
+  {
+      int counter = 0;
+      ax = 0;
+      ay = 0;
+      az = 0;
+      while(counter < 10)
+      {
+        getAcceleration(accelgyro,tmpAX,tmpAY,tmpAZ);
+        ax+=tmpAX;
+        ay+=tmpAY;
+        az+=tmpAZ;
+        counter++;
+        delay(5);
+        LED_loading();
+      }
+      ax /= 10;
+      ay /= 10;
+      az /= 10;
+      aGes = sqrt(ax*ax+ay*ay+az*az);
+      /*
+      Serial.print("average x:  ");
+      Serial.print(ax);
+      Serial.print("\ty:  ");
+      Serial.print(ay);
+      Serial.print("\tz:  ");
+      Serial.print(az);
+      Serial.print("\tges:");
+      Serial.print(aGes);
+      Serial.print("\t");
+      */
+      
+
+      error = aGes - toAZ;
+      error = sqrt(error*error);
+      
+      if(error < endError && sqrt(ax*ax)-toAX < endError && sqrt(ay*ay)-toAY < endError && sqrt(az*az)-toAZ < endError)
+      {
+        //Serial.print("End error: ");
+        //Serial.println(error);
+        ret = true;
+      }
+          
+      int16_t offsetX = (toAX-ax) * 10 + accelgyro.getXAccelOffset();
+      int16_t offsetY = (toAY-ay) * 10 + accelgyro.getYAccelOffset();
+      int16_t offsetZ = (toAZ-az) * 10 + accelgyro.getZAccelOffset();
+      /*
+      Serial.print("set to ofset: ax: ");
+      Serial.print(offsetX);
+      Serial.print("\tay: ");
+      Serial.print(offsetY);
+      Serial.print("\taz: ");
+      Serial.print(offsetZ);
+
+      Serial.print("\terror: ");
+      Serial.println(error);
+      */
+      accelgyro.setXAccelOffset(offsetX);
+      accelgyro.setYAccelOffset(offsetY);
+      accelgyro.setZAccelOffset(offsetZ);
+  }
+  for(int b=0; b<50; b++)
+  {
+     for(int a=0; a<NUMPIXELS; a++)
+     {
+        pixels.setPixelColor(a,0,b,0);
+     }
+     pixels.show();
+     delay(10);
+  }
+  for(int b=50; b>=0; b--)
+  {
+     for(int a=0; a<NUMPIXELS; a++)
+     {
+        pixels.setPixelColor(a,0,b,0);
+     }
+     pixels.show();
+     delay(10);
+  }
+}
+void getAcceleration(MPU6050 &accel,float &ax,float &ay, float &az)
+{
+  int16_t _ax;
+  int16_t _ay;
+  int16_t _az;
+  accel.getAcceleration(&_ax,&_ay,&_az);
+
+  ax = (float)_ax/(1024 * 0.8162);
+  ay = (float)_ay/(1024 * 0.8162);
+  az = (float)_az/(1024 * 0.8162);
+}
+void LED_loading()
+{
+  if(loadingTimer.start(20))
+  {
+    for(int a=0; a<NUMPIXELS; a++)
+    {
+      if(a == loadingPos || a == loadingPos-1)
+       pixels.setPixelColor(a,0,50,0);
+      else
+       pixels.setPixelColor(a,0,0,0);
+  
+       
+    }
+    loadingPos++;
+    if(loadingPos >= NUMPIXELS)
+    {
+      loadingPos = 1;
+    }
+    pixels.show();
+  }
+}
+void LED_akku()
+{
+    if(mode_akkuLowTimer.start(100))
+    {
+        for(int a=0; a<NUMPIXELS; a++)
+        {
+          pixels.setPixelColor(a,0,0,0);
+        }
+        byte pixelPos = map((int)((akkuVoltage-(minAkkuVoltage-1.0))*100),0,1100-(minAkkuVoltage-1.0)*100.0,1,NUMPIXELS);
+        for(int a=0; a<pixelPos; a++)
+        {
+          //map((long)(akkuVoltage*100)%NUMPIXELS,0,1223/NUMPIXELS,0,30)
+          if(mode_akkuLow_numLedOn > 25)
+            mode_akkuLow_numLedOn = 0;
+          if(a>mode_akkuLow_numLedOn)
+            continue;
+           int brightness = 100;
+           if(a == pixelPos-1)
+           {
+           // Serial.print("brightness: ");
+            // brightness = /*(1100-(akkuVoltage-(minAkkuVoltage-1.0))*100)*/(1100-(minAkkuVoltage-1.0)*100)  / NUMPIXELS;
+           //  Serial.println(brightness);
+           //  brightness = map(akkuVoltage*100-brightness*pixelPos,0,brightness*NUMPIXELS,0,100);
+            // brightness = map((1100-(akkuVoltage-(minAkkuVoltage-1.0))*100),0,brightness
+             brightness = (int)(1100-(minAkkuVoltage-1.0)*100)  / NUMPIXELS;
+            // Serial.print(brightness);
+             brightness = map((int)((akkuVoltage-(minAkkuVoltage-1.0))*100)% NUMPIXELS,0,brightness,0,100);
+           //  Serial.print(" ");
+           //  Serial.println(brightness);
+           }
+           pixels.setPixelColor(a,brightness*map(a,0,NUMPIXELS,30,0)/100,brightness*map(a,0,NUMPIXELS,0,30)/100,0);
+        }
+        pixels.show();
+        //if(mode_akkuLow_numLedOn < NUMPIXELS)
+        {
+          mode_akkuLow_numLedOn++;
+        }
+    }
+}
+void checkAkku()
+{
+  //V factor 0.6911764
+  int tmpV = analogRead(AKKUPIN);
+  akkuVoltage += (float)map(tmpV,0,1024,0,1223)/100;
+  akkuVoltage /= 2;
+  //Serial.print("voltage: ");
+  //Serial.println(akkuVoltage);
+  if(akkuVoltage < minAkkuVoltage)
+  {
+    //Serial.println("voltage is to low");
+    MODE(modus_akkuLow);
+    pixels_color.red = 0;
+    pixels_color.green = 0;
+    pixels_color.blue = 0;
+    pixelsUpdateTimer.autoRestart(false);
+    pixelsUpdateTimer.stop();
+  }
+  else
+  {
+    //MODE(mode_none);
+  }
 }
 
